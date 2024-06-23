e70ad322406911a454e1b589d3498d6bfd456404
chinyuchan <chinyuchan@163.com>
2024-06-23 23:33:37 +0800

    optimize.

--- multi-threading/src/main.rs
+++ multi-threading/src/main.rs
@@ -3,7 +3,7 @@ use std::thread;
 fn main() {
     let mut handles = vec![];
     for _ in 1..10 {
-        let h = thread::spawn(||{
+        let h = thread::spawn(|| {
             println!("Hi from thread id {:?}", thread::current().id());
         });
         handles.push(h);
@@ -11,13 +11,14 @@ fn main() {
 
     for i in 10..20 {
         let builder = thread::Builder::new().name(format!("mythread{}", i));
-        let h = builder.spawn(||{
-            println!("Hi from thread id {:?}", thread::current().name().unwrap());
-        }).unwrap();
+        let h = builder
+            .spawn(|| {
+                println!("Hi from thread id {:?}", thread::current().name().unwrap());
+            })
+            .unwrap();
         handles.push(h);
     }
     for h in handles {
         h.join().unwrap();
     }
-
 }
