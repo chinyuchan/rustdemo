55e118e7707d37b0a9849def83822592cb0782f1
chinyuchan <chinyuchan@163.com>
2024-06-23 23:32:51 +0800

    multithreading.

---
+++ multi-threading-mpsc/src/main.rs
@@ -0,0 +1,23 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (sender, receiver) = mpsc::channel();
+    let sender2 = mpsc::Sender::clone(&sender);
+
+    thread::spawn(move || {
+        for i in 1..100 {
+            sender.send(i).unwrap();
+        }
+    });
+
+    thread::spawn(move || {
+        for i in 100..200 {
+            sender2.send(i).unwrap();
+        }
+    });
+
+    for val in receiver {
+        println!("Received from thread: {}", val);
+    }
+}
---
+++ multi-threading/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+
+fn main() {
+    let mut handles = vec![];
+    for _ in 1..10 {
+        let h = thread::spawn(||{
+            println!("Hi from thread id {:?}", thread::current().id());
+        });
+        handles.push(h);
+    }
+
+    for i in 10..20 {
+        let builder = thread::Builder::new().name(format!("mythread{}", i));
+        let h = builder.spawn(||{
+            println!("Hi from thread id {:?}", thread::current().name().unwrap());
+        }).unwrap();
+        handles.push(h);
+    }
+    for h in handles {
+        h.join().unwrap();
+    }
+
+}
